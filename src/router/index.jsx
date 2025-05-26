6c6045ffb8143576685f9a46e7ea6d5413fc7d23
beze86 <arapi.besmir@gmail.com>
2025-05-26 09:54:52 +0200

    Fix route

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -37,7 +37,7 @@ export const router = createBrowserRouter([
                   element: <Cases />,
                },
                {
-                  path: "tecnologia-logistica",
+                  path: "logistics-technology",
                   element: <CasesLogistics />,
                }
             ]
