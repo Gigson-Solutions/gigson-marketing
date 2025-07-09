5a90e3f63aea036fdf757d964c2c6a673bc415b0
Alejandro <100495744@alumnos.uc3m.es>
2025-07-09 18:04:53 +0200

    Changed hero

--- src/components/Pages/Logistics/Logistics.jsx
+++ src/components/Pages/Logistics/Logistics.jsx
@@ -1,18 +1,27 @@
-import Hero from "./Hero.jsx";
-import UseCases from "./UseCases.jsx";
+
+import UseCases from "../../../shared/ui/UseCases.jsx";
 import useCasesBgGradient from '../../../assets/casos-de-uso-bg-gradient-1.svg';
+import Hero from "../../../shared/ui/Hero.jsx";
 
 import SolutionsApplications from "../../../shared/ui/SolutionsApplications.jsx";
 import ImproveAreas from "../../../shared/ui/ImproveAreas.jsx";
 import HowWeWork from "../../../shared/ui/HowWeWork.jsx";
 import DigitalProduct from "../../../shared/ui/DigitalProduct.jsx";
 import { Helmet } from 'react-helmet-async'
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 
 const Logistics = () => {
     const { t } = useTranslation();
 
-    const { title, metadescription} = t("cases-logistics");
+    const { title, metadescription, hero } = t("cases-logistics");
+
+    const Title = <Trans
+        i18nKey={hero.title}
+        components={{
+            span: <span className="text-purple-accents" />,
+            br: <br />
+        }}
+    />
 
     return (
         <>
@@ -20,7 +29,7 @@ const Logistics = () => {
                 <title>{title}</title>
                 <meta name="description" content={metadescription} />
             </Helmet>
-            <Hero />
+            <Hero title={Title} description={hero.description} suptitle={hero.suptitle} buttonText={hero.buttonText}/>
             <section style={{
                 backgroundImage: `url(${useCasesBgGradient}`,
                 backgroundPosition: 'center center',
--- src/shared/ui/Hero.jsx
+++ src/shared/ui/Hero.jsx
@@ -1,31 +1,19 @@
-import {Link} from "react-router-dom";
-import {ButtonLink} from "./Button.jsx";
-import {Trans, useTranslation} from "react-i18next";
+import { ButtonLink } from "./Button.jsx";
+import { Trans, useTranslation } from "react-i18next";
 
 const buttonLink = '/contact'
 
-const Hero = () => {
-
-    const {t} = useTranslation();
-    const {
-        hero: { title, suptitle,  description, buttonText}
-    } = t("cases-logistics");
+const Hero = ({ title, suptitle, description, buttonText }) => {
 
     return (
         <section className="mt-fixed-navbar pt-14 lg:pt-23 pb-36 lg:pb-23">
             <div className="max-w-8xl mx-auto px-landing flex flex-col items-center">
                 <p className="text-purple-accents text-body1 uppercase mb-4 md:mb-6">{suptitle}</p>
-                <h1 className="text-6xl text-dark-primary text-center mb-4 md:mb-6">
-                    <Trans
-                        i18nKey={title}
-                        components={{
-                            span: <span className="text-purple-accents" />,
-                            br: <br />
-                        }}
-                    />
+                <h1 className="text-h1 text-dark-primary text-center mb-4 md:mb-6">
+                    {title}
                 </h1>
                 <p className="text-dark-primary text-subtitle max-w-contained text-center mb-6 md:mb-10">{description}</p>
-                <ButtonLink link={buttonLink} text={buttonText} outlined/>
+                <ButtonLink link={buttonLink} text={buttonText} outlined />
             </div>
         </section>
     )
