12e4eaa140b3126512ebf3777467af53df189d5f
Jonay Hernández <jonay.hdez@gmail.com>
2025-07-16 13:17:03 +0100

    Replace <h3> and <h4> tags with <p> tags for title elements in DigitalProduct and HowWeWork components

--- src/shared/ui/HowWeWork.jsx
+++ src/shared/ui/HowWeWork.jsx
@@ -7,7 +7,7 @@ const Method = ({ title, description, isInnerMethod, isFirstCard}) => {
         <div className={`${boxClass} flex-col border-b gap-y-10 px-4 py-10 md:p-4 md:pb-6`}>
             {isInnerMethod ? (
                 <>
-                    <h4 className="text-h4 text-purple-accents">{title}</h4>
+                    <p className="text-h4 text-purple-accents">{title}</p>
                     <div className="flex items-start justify-between gap-x-10">
                         <p className="text-body text-purple-accents">{description}</p>
                     </div>
@@ -15,7 +15,7 @@ const Method = ({ title, description, isInnerMethod, isFirstCard}) => {
             ) : (
                 <>
                     <div className="flex items-center justify-between">
-                        <h4 className="text-h4 text-white">{title}</h4>
+                        <p className="text-h4 text-white">{title}</p>
                     </div>
                     <p className="hidden lg:block text-body text-white">{description}</p>
                 </>
@@ -30,9 +30,9 @@ const HowWeWork = ({title, methods}) => {
     return (
         <section className="py-14 lg:py-40 bg-gradient-to-br from-[#6d6ff6] via-[#6d6ff6] to-[#e4e5ff]">
             <div className="max-w-8xl mx-auto px-landing flex flex-col">
-                <h3 className="text-h2 text-white mb-6 md:mb-8 lg:mb-16">
+                <p className="text-h2 text-white mb-6 md:mb-8 lg:mb-16">
                     {title}
-                </h3>
+                </p>
 
                 <div className="flex flex-col lg:flex-row flex-wrap gap-10">
                     <div className="bg-purple-light flex flex-col items-center justify-center flex-8 lg:order-2">
