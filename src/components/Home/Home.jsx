cf2e39d7cc6d096505403c2b4367c0a085fa64dc
Alejandro <86131252+100495744@users.noreply.github.com>
2025-07-23 13:29:22 +0100

    Put helmet in Home (#19)

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,10 +4,19 @@ import HomeServices from "./HomeServices";
 import Form from "../Form";
 import HomeCases from "./HomeCases";
 import { CtaServices } from "./CtaServices";
+import { Helmet } from 'react-helmet-async'
+import { useTranslation } from "react-i18next";
 
 const Home = () => {
+   const { t } = useTranslation();
+   const {title , metadescription} = t("home")
+
    return (
       <div className="home-content">
+         <Helmet>
+                <title>{title}</title>
+                <meta name="description" content={metadescription} />
+            </Helmet>
          <Hero />
          <HomeServices />
          <HomeCases />
