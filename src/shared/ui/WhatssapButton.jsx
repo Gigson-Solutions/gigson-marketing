46fdc0092416a760c2eda247cc0d9d48a6fb4440
Alejandro <100495744@alumnos.uc3m.es>
2025-08-01 19:11:05 +0200

    changed to Library, put normal proms for now

--- src/shared/ui/WhatssapButton.jsx
+++ src/shared/ui/WhatssapButton.jsx
@@ -1,24 +1,14 @@
 import React from "react";
+import { FloatingWhatsApp } from "react-floating-whatsapp";
 
 import Icon from "../../assets/WhatsApp.svg"
 
 const WhatsappButton = () => {
-    const phoneNumber = "34623783507"; 
+    const Number = "34623783507"; 
     const message = "¡Hola! Quisiera más información."; 
-    const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    
     return (
-      <a
-      href={whatsappLink}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="fixed bottom-6 right-6 z-9000"
-    >
-      <img
-        src={Icon}
-        alt="WhatsApp"
-        className="w-14 h-14 hover:scale-110 transition-transform duration-300"
-      />
-    </a>
+       <FloatingWhatsApp  />
 
     )
 
