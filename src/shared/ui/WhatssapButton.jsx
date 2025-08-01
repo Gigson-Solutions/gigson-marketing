a06b72514d51350d75fefbcbf8264f68e170a46b
Alejandro <100495744@alumnos.uc3m.es>
2025-08-01 14:24:11 +0200

    Well made Icon and link

--- src/shared/ui/WhatssapButton.jsx
+++ src/shared/ui/WhatssapButton.jsx
@@ -3,13 +3,22 @@ import React from "react";
 import Icon from "../../assets/WhatsApp.svg"
 
 const WhatsappButton = () => {
-
+    const phoneNumber = "34623783507"; 
+    const message = "¡Hola! Quisiera más información."; 
+    const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     return (
-         <img
-        src= {Icon} 
+      <a
+      href={whatsappLink}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="fixed bottom-6 right-6 z-9000"
+    >
+      <img
+        src={Icon}
         alt="WhatsApp"
         className="w-14 h-14 hover:scale-110 transition-transform duration-300"
       />
+    </a>
 
     )
 
