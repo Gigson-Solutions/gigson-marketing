c3d6d501b493f055de3837052e6d7e87b321bebb
Alejandro <100495744@alumnos.uc3m.es>
2025-08-01 13:49:47 +0200

    First Definition , imported image

---
+++ src/shared/ui/WhatssapButton.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Icon from "../../assets/WhatsApp.svg"
+
+const WhatsappButton = () => {
+
+    return (
+         <img
+        src= {Icon} 
+        alt="WhatsApp"
+        className="w-14 h-14 hover:scale-110 transition-transform duration-300"
+      />
+
+    )
+
+
+
+}
+
+export default WhatsappButton
