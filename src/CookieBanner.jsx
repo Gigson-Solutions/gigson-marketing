a7c60f1e7a9fe5cd6d8885a43a9436fc566448aa
Alejandro <100495744@alumnos.uc3m.es>
2025-08-01 13:32:51 +0200

    Cambiado a que se reduzca solo en mobile

--- src/CookieBanner.jsx
+++ src/CookieBanner.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+
 import Cookies from 'js-cookie';
 import './CookieBanner.css'; // Añadimos estilos personalizados
 import { useTranslation } from 'react-i18next';
@@ -9,14 +10,13 @@ const CookieBanner = () => {
    const { t } = useTranslation();
    const { h2, p, btnAccept, btnDenie, btnInfo } = t('cookiesBanner');
    const navigate = useNavigate();
+
    const handleClick = () => {
       navigate("/cookies");
    }
-   
-   
 
    useEffect(() => {
-      // Verificar si el usuario ya ha aceptado o rechazado las cookies
+      
       const cookieConsent = Cookies.get('cookieConsent');
       if (!cookieConsent) {
          setIsVisible(true);
@@ -24,27 +24,27 @@ const CookieBanner = () => {
    }, []);
 
    const handleAccept = () => {
-      // Establecer cookie de consentimiento con valor 'accepted'
+     
       Cookies.set('cookieConsent', 'accepted', { expires: 365 });
       setIsVisible(false);
    };
 
    const handleReject = () => {
-      // Establecer cookie de consentimiento con valor 'rejected'
+      
       Cookies.set('cookieConsent', 'rejected', { expires: 365 });
       setIsVisible(false);
    };
 
-   if (!isVisible) return null; // No renderizar el banner si no es visible
+   if (!isVisible) return null; 
 
-   return (
+   return  (
       <div className="cookie-banner">
          <p>{h2}</p>
-         <p>{p}</p>
+         <p className='hidden md:block' >{p}</p>
          <div className="cookie-buttons">
             <button onClick={handleAccept}>{btnAccept}</button>
             <button onClick={handleReject}>{btnDenie}</button>
-            <button onClick={handleClick}>{btnInfo}</button>
+            <button onClick={handleClick} className='md:hidden'>{btnInfo}</button>
          </div>
       </div>
    );
