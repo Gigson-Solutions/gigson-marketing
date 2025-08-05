6b652584d9166d69eb3b536caf1432daaad4e4ff
Alejandro <100495744@alumnos.uc3m.es>
2025-08-05 13:23:17 +0200

    Put text and correct Image

--- src/components/Pages/Logistics/Logistics.jsx
+++ src/components/Pages/Logistics/Logistics.jsx
@@ -5,7 +5,6 @@ import useCasesBgGradient from 'assets/casos-de-uso-bg-gradient-1.svg';
 import SolutionsApplications from 'shared/SolutionsApplications.jsx';
 import ImproveAreas from 'shared/ImproveAreas.jsx';
 import HowWeWork from 'shared/HowWeWork.jsx';
-import Whatsapp from 'shared/WhatssapButton.jsx';
 import DigitalProduct from 'shared/DigitalProduct.jsx';
 import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
@@ -33,7 +32,7 @@ const Logistics = () => {
             <SolutionsApplications {...solutionsApplications} />
             <HowWeWork {...howWeWork} />
             <DigitalProduct {...digitalProduct}/>
-            <Whatsapp/>
+          
         </>
     );
 };
--- src/shared/ui/WhatssapButton.jsx
+++ src/shared/ui/WhatssapButton.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { FloatingWhatsApp } from "react-floating-whatsapp";
-import avatar from "../../assets/avatar.webp"
+import avatar from "../../assets/avatar.jpg"
 import { useTranslation } from 'react-i18next';
 
 
 const WhatsappButton = () => { 
     const { t } = useTranslation();
      
-    const Number = "623783507"; 
-    const message = "¡Hola! Quisiera más información."; 
     const {number , accountName, placeholder, statusmessage, chatMessage} = t("whatsapp");
     
     return (
