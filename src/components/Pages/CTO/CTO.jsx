450bb78fba54ceab56d2f1421575b5a966087655
Alejandro <100495744@alumnos.uc3m.es>
2025-08-06 11:26:57 +0200

    Made full page of CTO

---
+++ src/components/Pages/CTO/CTO.jsx
@@ -0,0 +1,29 @@
+import Hero from 'shared/Hero.jsx';
+import UseCases from 'shared/UseCases.jsx';
+import useCasesBgGradient from 'assets/casos-de-uso-bg-gradient-1.svg';
+
+import SolutionsApplications from 'shared/SolutionsApplications.jsx';
+import ImproveAreas from 'shared/ImproveAreas.jsx';
+import HowWeWork from 'shared/HowWeWork.jsx';
+import DigitalProduct from 'shared/DigitalProduct.jsx';
+import { Helmet } from 'react-helmet-async';
+import { useTranslation } from 'react-i18next';
+import Brand from 'shared/Brand.jsx';
+
+const CTO = () => {
+    const { t } = useTranslation();
+    const { hero, solutionsApplications, howWeWork, digitalProduct } = t('CTO');
+    
+    return (
+        <>
+            
+            <Hero {...hero}/>
+            <SolutionsApplications {...solutionsApplications} />
+            <HowWeWork {...howWeWork} />
+            <DigitalProduct {...digitalProduct}/>
+            <Brand />         
+        </>
+    );
+};
+
+export default CTO;
