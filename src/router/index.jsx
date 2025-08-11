6ffc972629485ed881d3aed81ead793c0991329f
Alejandro <86131252+100495744@users.noreply.github.com>
2025-08-11 16:02:49 +0200

    [WEB-79] cto as service page (#27)
    * Made full page of CTO
    
    * Page Finished for deploy
    
    * Update src/locales/en/translation.json
    
    Co-authored-by: Jonay Hernandez <62750325+JonayHdez@users.noreply.github.com>
    
    * Update src/locales/es/translation.json
    
    Co-authored-by: Jonay Hernandez <62750325+JonayHdez@users.noreply.github.com>
    
    * Update translation for solutionsApplications title to improve clarity
    
    * fixed
    
    ---------
    
    Co-authored-by: Jonay Hernandez <62750325+JonayHdez@users.noreply.github.com>
    Co-authored-by: Jonay Hernández <jonay.hdez@gmail.com>

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -9,6 +9,7 @@ import Logistics from '../components/Pages/Logistics/Logistics';
 import NotFound from '../components/Pages/NotFound/NotFound';
 import Policity from '../components/Pages/Policity';
 import CookiesPage from '../components/Pages/CookiesPage';
+import CTO from '../components/Pages/CTO/CTO'
 
 import {Navigate, Outlet, createBrowserRouter} from 'react-router-dom';
 import '../App.css';
@@ -26,6 +27,7 @@ const generateRoutes = (lang) => {
       { index: true, element: <Home /> },
       { path: slugs.services, element: <Services /> },
       { path: slugs.logistics,element: <Logistics /> },
+      { path: slugs.CTO,element: <CTO/> },
       { path: slugs.cases, element: <Cases /> },
       { path: slugs.about, element: <AboutHero /> },
       { path: slugs.faqs, element: <Faqs /> },
