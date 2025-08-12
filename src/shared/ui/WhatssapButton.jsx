f62247c76f110d930fcd49c67fce6b836022c423
Jonay Hernández <jonay.hdez@gmail.com>
2025-08-12 15:26:59 +0100

    Refactor WhatsappButton component to remove unnecessary style block and apply styles directly

--- src/shared/ui/WhatssapButton.jsx
+++ src/shared/ui/WhatssapButton.jsx
@@ -7,23 +7,18 @@ const WhatsappButton = () => {
     const whatsappData = t('whatsapp');
 
     return (
-    <>
-      <style>{`
-        
-        .floating-whatsapp-button::after {
-          animation: none !important;
-          box-shadow: none !important;
-          content: '' !important; 
-        }
-      `}</style>
-
       <FloatingWhatsApp
         allowClickAway
         avatar={avatar}
         {...whatsappData}
-       
+        style={{
+          '.floating-whatsapp-button::after': {
+            animation: 'none',
+            boxShadow: 'none',
+            content: ''
+          }
+        }}
       />
-    </>
   );
 };
 
