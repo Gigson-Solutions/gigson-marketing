807aa5d5b73fd3431ffc98896f2b94aa2d4732c6
Alejandro <86131252+100495744@users.noreply.github.com>
2025-08-19 09:01:24 +0100

    [WEB-129] changed animation whatsapp button (#35)
    * Changed Text and eliminated animation
    
    * Refactor WhatsappButton component to remove unnecessary style block and apply styles directly
    
    * Added Css for change in animation
    
    ---------
    
    Co-authored-by: Jonay Hernández <jonay.hdez@gmail.com>

--- src/shared/ui/WhatssapButton.jsx
+++ src/shared/ui/WhatssapButton.jsx
@@ -1,17 +1,20 @@
 import { FloatingWhatsApp } from 'react-floating-whatsapp';
 import avatar from '../../assets/avatar.jpg';
 import { useTranslation } from 'react-i18next';
+import './WhatsappButton.css';
 
 const WhatsappButton = () => {
     const { t } = useTranslation();
     const whatsappData = t('whatsapp');
 
     return (
-        <FloatingWhatsApp allowClickAway
-            avatar={avatar}
-            {...whatsappData}
-        />
-    );
+      <FloatingWhatsApp
+        allowClickAway
+        avatar={avatar}
+        {...whatsappData}
+      
+      />
+  );
 };
 
 export default WhatsappButton;
